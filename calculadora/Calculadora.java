f683197ddffacf47700609574eac23986178c045
André Vitor <salesdias1207@gmail.com>
2026-05-15 19:43:26 -0300

    refactor: Etapa 4 - Refatoração do Código (Switch e Extração de Métodos)

--- calculadora/Calculadora.java
+++ calculadora/Calculadora.java
@@ -1,25 +1,36 @@
 package calculadora;
 
 public class Calculadora {
-    public int r;
 
-    public int calc(int a, int b, String op) {
-        if (op.equals("+")) {
-            r = a + b;
-        } else if (op.equals("-")) {
-            r = a - b;
-        } else if (op.equals("*")) {
-            r = a * b;
-        } else if (op.equals("/")) {
-            if (b == 0) {
-                System.out.println("Erro: Divisão por zero não permitida!");
+    public int calc(int a, int b, String operador) {
+        switch (operador) {
+            case "+": return somar(a, b);
+            case "-": return subtrair(a, b);
+            case "*": return multiplicar(a, b);
+            case "/": return dividir(a, b);
+            default:
+                System.out.println("Operação inválida!");
                 return 0;
-            }
-            r = a / b;
-        } else {
-            System.out.println("Operação inválida!");
+        }
+    }
+
+    private int somar(int a, int b) {
+        return a + b;
+    }
+
+    private int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    private int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    private int dividir(int a, int b) {
+        if (b == 0) {
+            System.out.println("Erro: Divisão por zero não permitida!");
             return 0;
         }
-        return r;
+        return a / b;
     }
 }
